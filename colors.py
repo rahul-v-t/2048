0b679e886b1f1ce772c44d4e7de2df63e6f13712
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-05 15:51:11 +0530

    New One

---
+++ colors.py
@@ -0,0 +1,32 @@
+GRID_COLOR = "#a39489"
+EMPTY_CELL_COLOR = "#c2b3a9"
+WINNER_BG = "ffcc00"
+LOSER_BG = "a39489"
+
+CELL_COLOURS = {
+    2: "#fcefe6",
+    4: "#f2e8cb",
+    8: "#f5b682",
+    16: "#f29446",
+    32: "#ff775c",
+    64: "#e64c2e",
+    128: "#ede291",
+    256: "#fce130",
+    512: "#ffdb4a",
+    1024: "#f0b922",
+    2048: "#fad74d"
+}
+
+CELL_NUMBER_COLOURS = {
+    2: "#695c57",
+    4: "#695c57",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
